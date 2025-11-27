08d4c5f82f1d7ecc2ffc5901d8c4850f59dbb062
dhanushr9625 <dhanushdhanush3579@gmail.com>
2025-11-27 19:40:56 +0530

    Add files via upload

---
+++ unit_2/InterfaceDemo.java
@@ -0,0 +1,22 @@
+interface Drawable {
+    void draw(); 
+}
+class Circle implements Drawable {
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+class Rectangle implements Drawable {
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Drawable d1 = new Circle();
+        Drawable d2 = new Rectangle();
+        
+        d1.draw(); 
+        d2.draw();
+    }
+}
